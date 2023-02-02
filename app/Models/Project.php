7ac15d24ace28a24cabe8c97f195cd0a01927e87
andreadldev <andrea.delisi2@gmail.com>
2023-02-02 10:53:59 +0100

    Aggiungi url immagini nel modello

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -4,11 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 
 class Project extends Model
 {
     use HasFactory;
     protected $guarded = ['slug', 'technologies'];  
+    protected $appends = ['image_url'];
+
+    protected function getImageUrlAttribute() {
+        return $this->image ? asset("storage/$this->image") : "https://via.placeholder.com/150";
+    }
 
     public function type() {
         return $this->belongsTo(Type::class);
